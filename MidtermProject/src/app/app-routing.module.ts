d38a25d2e49420a7db695b5625a87cdbf0d567cc
Rashitov Ilyas <rashtv.19@gmail.com>
2023-10-24 18:19:23 +0600

    home- feed- about_us- components added | routing added

--- MidtermProject/src/app/app-routing.module.ts
+++ MidtermProject/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'feed', component: FeedComponent },
+  { path: 'about', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MidtermProject/src/app/app.module.ts
+++ MidtermProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeedComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
